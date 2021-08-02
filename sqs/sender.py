a70b82a3932b0b73b8fbc534ec3be7b39ebcb1d9
kimsehwan96 <sktpghks138@gmail.com>
2021-08-02 16:59:45 +0900

    wip

--- sqs/sender.py
+++ sqs/sender.py
@@ -2,8 +2,23 @@ import boto3
 import os
 
 QUEUE_NAME = os.environ.get('QUEUE_NAME', 'PocQueue')
+QUEUE_URL = os.environ.get('QUEUE_URL')
 
 client = boto3.client('sqs')
 
 def handler(event, context):
-    print(event, context)
+    print(event)
+    if event.get('body'):
+        try:
+            res = client.send_message(
+                QueueUrl=QUEUE_URL,
+                MessageBody=event.body
+            )
+            print(res)
+            return res
+        except Exception as e:
+            print(e)
+            return "fail"
+    else:
+        print('there is no body in this request.')
+        return "fail"
